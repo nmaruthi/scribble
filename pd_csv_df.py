2d6214a0f44411712f62fdd9a56304d66b5f7b40
Nitesh Maruthi <nit.official@gmail.com>
2019-02-04 18:04:14 -0600

    Merging changes from local

--- pd_csv_df.py
+++ pd_csv_df.py
@@ -6,3 +6,9 @@ brics=pd.read_csv(data_sets_path+separator+'brics.csv', index_col=0)
 #Subsetting the data frame to specific rows & columns
 #This returns a data frame since double-square brackets are used
 brics.loc[['RU', 'IN', 'SA'], ['country', 'capital']]
+
+#Fetch all rows, specific columns
+brics.loc[:, ['country', 'capital']]
+
+#Using square brackets only to subset rows - returns a dataframe
+brics[0:3]
